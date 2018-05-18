2a2a2f4214ef93a37c83a656c434fc8a3e40029b
Pranav Nutalapati <pranavnutalapati@gmail.com>
2018-05-18 09:05:59 +0530

    Now works with spaces as well as tabs. Reindents everything to a consistent format so that all image look similar

--- test_file.js
+++ test_file.js
@@ -1,24 +1,24 @@
 _ = (sel) => (e = document.querySelectorAll(sel), e.length > 1 ? e :e[0])
 if (localStorage.getItem('previous-page') == 'home') {
-	_('.transition').classList.add('to-left');
-	setTimeout(function() {
-		_('.transition').classList.remove('to-left');
-		_('.transition').classList.remove('show');
-	}, 500)
+    _('.transition').classList.add('to-left');
+    setTimeout(function() {
+        _('.transition').classList.remove('to-left');
+        _('.transition').classList.remove('show');
+    }, 500)
 } else {
-	_('.transition').classList.remove('show');
+    _('.transition').classList.remove('show');
 }
 localStorage.setItem('previous-page', 'login')
 
 _("#login-form").addEventListener('submit', function(e) {
-	e.preventDefault();
-	var username = _('#username').value,
-		password = _('#password').value;
+    e.preventDefault();
+    var username = _('#username').value,
+        password = _('#password').value;
 
-	// Do some submit logic here!
-	// Okay by now the user is verified.
+    // Do some submit logic here!
+    // Okay by now the user is verified.
 
-	_('.transition').classList.add('show');
-	_('.transition').classList.add('from-left');
-	setTimeout(function() {window.location.href = 'home'}, 500)
+    _('.transition').classList.add('show');
+    _('.transition').classList.add('from-left');
+    setTimeout(function() {window.location.href = 'home'}, 500)
 })
